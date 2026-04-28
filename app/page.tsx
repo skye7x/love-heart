aeb7610c8abda0d1a2e62d982337e509c0a32ccc
Bartek <80260053+skye7x@users.noreply.github.com>
2026-04-28 12:40:11 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import LoveHeart from "@/components/LoveHeart";
+
+export default function Home() {
+  return (
+    <main
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        background: "#0a0005",
+      }}
+    >
+      <LoveHeart />
+    </main>
+  );
+}
